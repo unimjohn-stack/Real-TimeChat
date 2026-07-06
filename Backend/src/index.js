543315ad927a130d20d0f226dbfb483ad48f6f9c
unimjohn <johniduku@gmail.com>
2026-07-06 20:04:46 +0100

    commit docker

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,9 @@
 // const express = require("express");
 import express from "express";
 // import dotenv from "dotenv"
-import "dotenv/config"
+import "dotenv/config";
+import fs from "fs";
+import path from "path";
 import { connectDB } from "./lib/db.js";
 // const PORT = 3000;
 import { clerkMiddleware } from '@clerk/express';
@@ -11,6 +13,7 @@ const app = express();
 
 const PORT = process.env.PORT;
 const FRONTENDURL = process.env.FRONTENDURL;
+const publicDir = path.join(process.cwd(), "public")
 // middleWare
 app.use(express.json());
 app.use(cors({origin:FRONTENDURL, credentials: true}));
@@ -19,7 +22,14 @@ app.use(clerkMiddleware());
 
 app.get("/health", (req,res) => {
     res.status(200).json({ ok: true });
-})
+});
+
+if(fs.existsSync(publicDir)){
+    app.use(express.static(publicDir));
+    app.get("/{*any}", (req,res,next) => {
+        res.sendFile(path.join(publicDir,"index.html"), (err) => (err));
+    });
+}
 
 app.listen(PORT, () => {
     connectDB();
