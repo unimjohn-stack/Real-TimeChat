978689165651350cb03295a97de29df45e1f0258
unimjohn <johniduku@gmail.com>
2026-07-07 21:56:46 +0100

    kry

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,9 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/react'
 
+const key = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+console.log("Clerk key:", key);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider>
+    <ClerkProvider publishableKey={key}>
       <App />
     </ClerkProvider>
   </StrictMode>,
