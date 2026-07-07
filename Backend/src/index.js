609f6bfd3cfaf81c36a63c788c74f30543033092
unimjohn <johniduku@gmail.com>
2026-07-07 21:34:17 +0100

    cronjob send request every 14 minutes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -8,6 +8,7 @@ import { connectDB } from "./lib/db.js";
 // const PORT = 3000;
 import { clerkMiddleware } from '@clerk/express';
 import cors from 'cors'
+import job from "./lib/cron.js";
 
 const app = express();
 
@@ -34,4 +35,8 @@ if(fs.existsSync(publicDir)){
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server is running on port ${PORT}`)
+    
+    if (process.env.NODE_ENV === "production") {
+        job.start()
+    }
 })
