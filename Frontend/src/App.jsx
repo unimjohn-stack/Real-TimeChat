cec4d0d991b3ca2214f2bf9f1f4a02777b981ae1
unimjohn <johniduku@gmail.com>
2026-07-13 18:19:13 +0100

    implemented frontend socket and setup for chatpage

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 // import './App.css'
-import { Show, SignIn, SignInButton, SignUpButton, UserButton } from '@clerk/react'
-import Home from './Pages/Home'
-import { Button } from '@heroui/react'
+// import { Show, SignIn, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+// import Home from './Pages/Home'
+// import { Button } from '@heroui/react'
 import { ThemeProvider } from './context/ThemeContext'
 import { WallpaperProvider } from './context/WallpaperContext'
 import { Routes, Route, Navigate } from 'react-router'
@@ -9,13 +9,31 @@ import ChatPage from './Pages/ChatPage'
 import AuthPage from './Pages/AuthPage'
 import { useAuth } from '@clerk/react'
 import PageLoader from './Components/PageLoader'
+import { useAuthStore } from './store/useAuthStore'
+import { useEffect } from 'react'
+import { Toaster } from 'react-hot-toast';
 
 function App() {
 
   const { isSignedIn, isLoaded } = useAuth();
 
+  // const { checkAuth, isCheckingAuth, clearAuth } = useAuthStore();
+
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+  const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    if (isSignedIn) checkAuth();
+
+    else clearAuth();
+
+  },[checkAuth, clearAuth, isLoaded, isSignedIn]);
   // todo...
-  if (!isLoaded) return <PageLoader />;
+  if (!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader />;
+  // axiosInstance.post("/api")
 
   return (
     <ThemeProvider>
@@ -24,6 +42,7 @@ function App() {
           <Route path='/' element={ isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace/>} />
           <Route path='/auth' element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace/>} />
         </Routes>
+        <Toaster />
       </WallpaperProvider>
     </ThemeProvider>
   )
